299fe40b3e5997f9c9bf49d0a0724bfe50fc4611
Eldar Nugaev <reldan0@gmail.com>
2025-02-20 00:24:20 -0800

    add steam

--- main.c
+++ main.c
@@ -13,6 +13,84 @@
 #define SCREEN_WIDTH    800
 #define SCREEN_HEIGHT   600
 
+typedef struct {
+    int x;
+    int y;
+} vec2_t;
+
+typedef struct
+{
+    int size;
+    int height[256];
+} ceiling_t;
+
+typedef struct {
+    int x;
+    int y;
+} drop_t;
+
+typedef struct {
+    ceiling_t ceiling;
+    int drops_count;
+    drop_t drops[20000];
+} scene_t;
+
+ceiling_t C_Init()
+{
+    ceiling_t ceiling;
+    ceiling.size = 256;
+    for (int i = 0; i < ceiling.size; i++) {
+        // Define the range
+        int min = 0;
+        int max = 255;
+
+        // Generate a random number within the range
+        char random_number = (rand() % (max - min + 1)) + min;
+        ceiling.height[i] = random_number;
+    }
+    return ceiling;
+}
+
+scene_t S_Init()
+{
+    scene_t scene;
+    scene.ceiling = C_Init();
+    scene.drops_count = 20000;
+    for  (int i = 0; i < scene.drops_count; i++) {
+        scene.drops[i].x = 0;
+        scene.drops[i].y = 1000;
+    }
+    return scene;
+}
+
+void C_AddSteam(scene_t *scene) {
+    int free_drop = -1;
+    ceiling_t *ceiling = &scene->ceiling;
+    for (int i = 0; i < ceiling->size; i++) {
+        if (rand() % 2 == 1) {
+            ceiling->height[i] = ceiling->height[i] + 1;
+        }
+        if (ceiling->height[i] > 100) {
+            ceiling->height[i] = 0;
+            for (;free_drop < scene->drops_count; free_drop++) {
+                if (free_drop > 0 && scene->drops[free_drop].y >= 500) {
+                    scene->drops[free_drop].y = 100;
+                    scene->drops[free_drop].x = i;
+                    break;
+                }
+            }
+            if (free_drop == scene->drops_count) {
+                printf("Free drops\n");
+            }
+        }
+    }
+    for (int i = 0; i < scene->drops_count; i++) {
+        if (scene->drops[i].y < 500) {
+            scene->drops[i].y = scene->drops[i].y + 1;
+        }
+    }
+}
+
 void close_sdl(SDL_Window* window) {
     if (window != NULL) {
         SDL_DestroyWindow(window);
@@ -22,6 +100,7 @@ void close_sdl(SDL_Window* window) {
 
 int main(int argc, char* argv[])
 {
+    srand(time(NULL));
     // Unused argc, argv
     (void) argc;
     (void) argv;
@@ -34,16 +113,6 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-#if defined linux && SDL_VERSION_ATLEAST(2, 0, 8)
-    // Disable compositor bypass
-    if(!SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0"))
-    {
-        printf("SDL can not disable compositor bypass!\n");
-        return 0;
-    }
-#endif
-
-    // Create window
     SDL_Window *window = SDL_CreateWindow("Basic C SDL project",
                                           SDL_WINDOWPOS_UNDEFINED,
                                           SDL_WINDOWPOS_UNDEFINED,
@@ -70,17 +139,7 @@ int main(int argc, char* argv[])
         close_sdl(window);
     }
 
-    // Declare rect of square
-    SDL_Rect squareRect;
-
-    // Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
-    squareRect.w = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-    squareRect.h = MIN(SCREEN_WIDTH, SCREEN_HEIGHT) / 2;
-
-    // Square position: In the middle of the screen
-    squareRect.x = SCREEN_WIDTH / 2 - squareRect.w / 2;
-    squareRect.y = SCREEN_HEIGHT / 2 - squareRect.h / 2;
-
+    scene_t scene = S_Init();
 
     // Event loop exit flag
     bool quit = false;
@@ -98,47 +157,57 @@ int main(int argc, char* argv[])
             quit = true;
         }
 
-        int min, max, random_number;
-
-        // Seed the random number generator with the current time
-        srand(time(NULL));
-
-        // Define the range
-        min = 0;
-        max = 255;
-
-        // Generate a random number within the range
-        random_number = (rand() % (max - min + 1)) + min;
-
-        // Print the random number
-        printf("Random number between %d and %d: %d\n", min, max, random_number);
-
-
         // Initialize renderer color white for the background
         SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
         // Clear screen
         SDL_RenderClear(renderer);
 
-        // Set renderer color red to draw the square
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, random_number, 0xFF);
+        int width = SCREEN_WIDTH / scene.ceiling.size;
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xAA, 0xFF);
 
-        // Draw filled square
-        SDL_RenderFillRect(renderer, &squareRect);
+        for (int i = 0; i < scene.ceiling.size; i++) {
+            int height = scene.ceiling.height[i];
 
-        for (int x = 0; x < SCREEN_WIDTH; x++) {
-            SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
-            SDL_RenderDrawPoint(renderer, x + random_number, x);
+            // Declare rect of square
+            SDL_Rect squareRect;
+
+            squareRect.x = i * width;
+            squareRect.y = 0;
+
+            squareRect.w = width;
+            squareRect.h = height;
+
+            SDL_RenderDrawRect(renderer, &squareRect);
         }
+
+        for  (int i = 0; i < scene.drops_count; i++) {
+            drop_t drop = scene.drops[i];
+            if  (drop.y < 500) {
+                int height = 10;
+
+                // Declare rect of square
+                SDL_Rect squareRect;
+
+                squareRect.x = drop.x * width;
+                squareRect.y = drop.y;
+
+                squareRect.w = width;
+                squareRect.h = height;
+                SDL_RenderDrawRect(renderer, &squareRect);
+            }
+        }
+
 
         // Update screen
         SDL_RenderPresent(renderer);
+        C_AddSteam(&scene);
+        SDL_Delay(10);
     }
 
     // Destroy renderer
     SDL_DestroyRenderer(renderer);
-
-   close_sdl(window);
+    close_sdl(window);
 
     return 0;
 }
